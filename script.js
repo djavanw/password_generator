d17c7bd6d206b46c3a1eb4d7ea9c2a24e97c5a26
Javan <71285290+djavanw@users.noreply.github.com>
2021-02-21 21:15:28 -0700

    Cleaned up the number array.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var daRandom = {                    //This is the criteria selection initial arrays.
   lowwer: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
   upper: ['A','B','C','D','E','F','G','H','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z'],   //Ommitted I and O.
-  numbers: [0,1,2,3,4,5,6,7,8,9,],
+  numbers: [0,1,2,3,4,5,6,7,8,9],
   charspec: ['!','@','#','$','%','^','/','&','*','?']
 }
 
